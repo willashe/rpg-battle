86062720affeb383464bbc59294eec93ce912543
Tomas Ortiz <tomas395@yahoo.com>
2021-09-12 02:21:54 -0700

    added a temp title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home  from "./components/Home";
 import battlePage from "./components/battlePage";
 import './App.css';
