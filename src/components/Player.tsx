a2902de8b3cdf4e2c4553525d9c1894017c204e1
Will Ashe <brantashe@gmail.com>
2021-09-12 20:52:57 -0500

    add prettier config and format all components

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Player = () => <h1>Player name, attributes, inventory and etc go here</h1>;
+const Player = () => (
+  <h1>Player name, attributes, inventory and etc go here</h1>
+);
 
 export default Player;
