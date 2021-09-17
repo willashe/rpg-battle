1d9268a3046a57ddf265674c6a4f8cd267b62374
Tomas Ortiz <tomas395@yahoo.com>
2021-09-16 20:16:46 -0700

    minor css change for the battle screen

--- src/components/Battle.tsx
+++ src/components/Battle.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Battle = () => (
   <div className="battle-container">
     <section className="monster-info-section">
-    <h1>Monster info section</h1>
-    <div className="l-monster-content">
-      <div className="l-monster-name">Left monster name</div>
-      <div className="l-monster-dmg">Left monster atk numbers</div>
-    </div>
-    <div className="r-monster-content">
-      <div className="r-monster-name">Right monster name</div>
-      <div className="r-monster-dmg">Right monster atk numbers</div>
-    </div>
+      <h1>Monster info section here</h1>
+      <div className="l-monster-content">
+        <div className="l-monster-name">Left monster name</div>
+        <div className="l-monster-dmg">Left monster atk numbers</div>
+      </div>
+      <div className="r-monster-content">
+        <div className="r-monster-name">Right monster name</div>
+        <div className="r-monster-dmg">Right monster atk numbers</div>
+      </div>
     </section>
 
     <section className="main-battle-section">
@@ -21,11 +21,13 @@ const Battle = () => (
     <section className="player-info-section">
       <h1>Party info section</h1>
       <div className="main-party-menu">
-        <span>p1</span>
-        <span>p2</span>
-        <span>p3</span>
-        <span>p4</span>
-        <p></p>
+        <p>p1&nbsp;</p>
+        <div className="player-hp-text">
+          <p>HP:&nbsp;20&nbsp;</p>
+        </div>
+        <div className="player-mp-text">
+          <p>MP:&nbsp;35&nbsp;</p>
+        </div>
       </div>
     </section>
   </div>
