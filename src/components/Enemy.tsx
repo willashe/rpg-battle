a20df6a580feaca077697153193a20dec45b6532
Will Ashe <brantashe@gmail.com>
2021-09-22 20:43:08 -0500

    add temp Enemy and Hero components

---
+++ src/components/Enemy.tsx
@@ -0,0 +1,34 @@
+interface EnemyProps {
+  index: number;
+  name: string;
+  status: string;
+  hp: number;
+  handleClick: () => void;
+}
+
+const Enemy = ({ index, name, status, hp, handleClick }: EnemyProps) => (
+  <button
+    disabled={hp <= 0}
+    onClick={() => handleClick()}
+    style={{
+      margin: '0 auto',
+      height: 130,
+      width: 90,
+      color: 'black',
+      background:
+        status === 'hurt' ? 'red' : status === 'dead' ? 'black' : 'white',
+      transformOrigin: 'bottom right',
+      transform: `rotate(${status === 'dead' ? 90 : 0}deg)`,
+      animation:
+        status === 'hurt' || status === 'dying' ? 'shake 0.5s' : undefined,
+      animationIterationCount:
+        status === 'hurt' || status === 'dying' ? 'infinite' : undefined,
+      border: 'none',
+    }}
+  >
+    <div>{name}</div>
+    <div>HP: {hp}</div>
+  </button>
+);
+
+export default Enemy;
