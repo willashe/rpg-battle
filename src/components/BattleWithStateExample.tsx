db3007322ee5e22639e72839082ea2b863ccc05c
Will Ashe <brantashe@gmail.com>
2021-09-22 20:43:27 -0500

    shitcode

--- src/components/BattleWithStateExample.tsx
+++ src/components/BattleWithStateExample.tsx
@@ -1,23 +1,127 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { AppStateContext } from '../state';
-import { actions } from '../actions';
+import { actionsCreators } from '../actions';
+import { EntityType } from '../types';
+import { EntityTypesEnum } from '../constants';
+import Hero from './Hero';
+import Enemy from './Enemy';
+const { MONSTER, ROBOT } = EntityTypesEnum;
 
-const { startNewGame, gameOver } = actions;
+const { startNewGame, attackThunk, deathThunk, setActiveHero, queueAction } =
+  actionsCreators;
 
 const Battle = () => {
   const [state, dispatch] = useContext(AppStateContext);
-  const { heroes, enemies } = state;
+  const { message, heroes, enemies, activeHero, targetType, queue } = state;
 
-  console.log(heroes);
-  console.log(enemies);
+  useEffect(() => {
+    enemies.left.forEach(({ status, hp }, i) => {
+      if (status !== 'dying' && status !== 'dead' && hp <= 0) {
+        dispatch(deathThunk('left', i));
+      }
+    });
+    enemies.right.forEach(({ status, hp }, i) => {
+      if (status !== 'dying' && status !== 'dead' && hp <= 0) {
+        dispatch(deathThunk('right', i));
+      }
+    });
+  }, [enemies, dispatch]);
+
+  console.log(queue);
 
   return (
     <>
       <h1>Battle!</h1>
 
       <button onClick={() => dispatch(startNewGame())}>New Game</button>
-      <button onClick={() => dispatch(gameOver())}>Game Over</button>
+      <br />
+
+      <h3>{message}</h3>
+
+      <div style={{ display: 'flex' }}>
+        {Boolean(heroes.length) && (
+          <>
+            {heroes.map((data: EntityType, index) => (
+              <Hero
+                key={index}
+                index={index}
+                active={activeHero === index}
+                handleClick={() => dispatch(setActiveHero(index))}
+                {...data}
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      {!!activeHero && !targetType && (
+        <>
+          <button
+            onClick={() =>
+              dispatch(
+                queueAction({
+                  heroIndex: activeHero,
+                  target: MONSTER,
+                  action: attackThunk(
+                    'left',
+                    enemies.left.findIndex(({ hp }) => hp >= 0)
+                  ),
+                })
+              )
+            }
+          >
+            Left
+          </button>
+          <button
+            onClick={() =>
+              dispatch(
+                queueAction({
+                  heroIndex: activeHero,
+                  target: ROBOT,
+                  action: attackThunk(
+                    'right',
+                    enemies.right.findIndex(({ hp }) => hp >= 0)
+                  ),
+                })
+              )
+            }
+          >
+            Right
+          </button>
+        </>
+      )}
+
+      {Boolean(queue.length) && (
+        <button onClick={() => dispatch(queue[0].action)}>Execute!</button>
+      )}
+
+      <div style={{ display: 'flex' }}>
+        {Boolean(enemies.left.length) && (
+          <>
+            {enemies.left.map((data: EntityType, index) => (
+              <Enemy
+                key={index}
+                index={index}
+                handleClick={() => dispatch(attackThunk('left', index))}
+                {...data}
+              />
+            ))}
+          </>
+        )}
+        {Boolean(enemies.right.length) && (
+          <>
+            {enemies.right.map((data: EntityType, index) => (
+              <Enemy
+                key={index}
+                index={index}
+                handleClick={() => dispatch(attackThunk('right', index))}
+                {...data}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 };
