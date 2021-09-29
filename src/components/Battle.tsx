b076e3c4bccfa31aa4934d9e9bc9ca3a045240cd
Tomas Ortiz <tomas395@yahoo.com>
2021-09-28 18:43:08 -0700

    updated styling on menu and battle screen

--- src/components/Battle.tsx
+++ src/components/Battle.tsx
@@ -8,20 +8,40 @@ const Battle = () => (
         <div className="monster-dmg">&nbsp;99</div>
       </div>
       <div className="monster-content">
-        <div className="monster-name">&nbsp;ROBOT</div>
-        <div className="monster-dmg">&nbsp;20</div>
+        <div className="monster-name">ROBOT</div>
+        <div className="monster-dmg">-</div>
       </div>
     </section>
 
     <section className="main-battle-section">
-      <h1>Fight screen here!</h1>
+      <div className="ability-used-window">
+        <p>NAFOI</p>
+      </div>
     </section>
 
     <section className="player-info-section">
       <div className="main-party-menu">
         <p>p1&nbsp;</p>
         <div className="player-hp-text">
-          <p>HP:&nbsp;20&nbsp;</p>
+          <p>HP:&nbsp;45</p>
+          <p>MP:&nbsp;33</p>
+          <p>ROLF</p>
+        </div>
+        <div className="player-hp-text">
+          <p>HP:&nbsp;✞</p>
+          <p>MP:&nbsp;0</p>
+          <p>NEI</p>
+        </div>
+        <div className="player-hp-text">
+          <p>ATTK</p>
+          <button className="player-button"></button>
+          <p>ORDR</p>
+          <button className="player-button"></button>
+        </div>
+        <div className="player-hp-text">
+          <p>HP:&nbsp;70</p>
+          <p>MP:&nbsp;0</p>
+          <p>RUDO</p>
         </div>
         <div className="player-mp-text">
           <p>MP:&nbsp;35&nbsp;</p>
